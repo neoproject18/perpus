c14021d3f13b877782f8d4a072284bae35ef5ff2
dharmabekti <dharmabekti@gmail.com>
2021-02-22 10:05:04 +0700

    export pdf

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -3,20 +3,14 @@
 use Dompdf\Dompdf;
 class Pdf extends Dompdf {
 
-    public $filename;
+    public $filename = '';
     public function __construct(){
         parent::__construct();
-        $this->filename = "laporan.pdf";
-    } 
-
-    protected function ci()
-    {
-        return get_instance();
     }
 
     public function load_view($view, $data = array())
     {
-        $html = $this->ci()->load->view($view, $data, TRUE);
+        $html = get_instance()->load->view($view, $data, TRUE);
         $this->load_html($html);
         // Render the PDF
         $this->render();
